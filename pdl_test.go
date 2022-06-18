c6ce7067429ec69b36ae9bdfa7e0b0b6ca4524c7
benjamin.ohana <ben@IL-PF2XRT9Q.corp.acronis.com>
2022-06-18 15:42:05 +0300

    add checksum (etag) feature

---
+++ pdl_test.go
@@ -0,0 +1,42 @@
+package pdl
+
+import (
+	"runtime"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestGetUrlInfo_ContentLength(t *testing.T) {
+	const testURL = "https://s3.amazonaws.com/nyc-tlc/trip+data/green_tripdata_2018-03.parquet"
+	urlInfo, err := getUrlInfo(testURL)
+	require.NoError(t, err)
+	expected := int64(13304186)
+	require.Equal(t, expected, urlInfo.ContentLength)
+}
+
+func TestGetUrlInfo_Etag(t *testing.T) {
+	const testURL = "https://s3.amazonaws.com/nyc-tlc/trip+data/green_tripdata_2018-03.parquet"
+	urlInfo, err := getUrlInfo(testURL)
+	require.NoError(t, err)
+	expected := "6ab45198235ba77c5b99a2e68a030d7f"
+	require.Equal(t, expected, urlInfo.ETag)
+}
+
+func TestGetMd5(t *testing.T) {
+	_, filename, _, _ := runtime.Caller(0)
+	t.Logf("Current test filename: %s", filename)
+	md5, err := getMd5("testResources/test.txt")
+	require.NoError(t, err)
+	require.Equal(t, "e80b5017098950fc58aad83c8c14978e", md5)
+}
+
+func TestDownloadPart(t *testing.T) {
+	const testURL = "https://s3.amazonaws.com/nyc-tlc/trip+data/green_tripdata_2018-03.parquet"
+	const testFile = "tests/part"
+	err := createEmptyFile(testFile, 50)
+	require.NoError(t, err)
+
+	err = downloadPart(testURL, 0, 50, testFile)
+	require.NoError(t, err)
+}
